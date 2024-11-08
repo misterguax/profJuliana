17769ecaecdecfe5ff9584a4a0eef37f4d74b24f
misterguax <124396846+misterguax@users.noreply.github.com>
2024-11-08 16:18:44 -0300

    Add files via upload

---
+++ jasmine-standalone-5.4.0/spec/testespec.js
@@ -0,0 +1,94 @@
+//NO testespec.js vai ficar o teste
+
+// it("Linha de teste", function (){
+//     expect(inteiro(1)).toBe(1);
+// })
+
+// describe("Teste de números", function(){
+//     it("Fizz Buzz", function(){
+//         expect(jogo(1)).toBe(1);
+//         expect(jogo(2)).toBe(2);
+//         expect(jogo(3)).toBe("Fizz");
+//         expect(jogo(4)).toBe(4);
+//         expect(jogo(5)).toBe("Buzz");
+//         expect(jogo(6)).toBe("Fizz");
+//         expect(jogo(7)).toBe(7);
+//         expect(jogo(8)).toBe(8);
+//         expect(jogo(9)).toBe("Fizz");
+//         expect(jogo(10)).toBe("Buzz");
+//     })
+// })
+
+// describe ("teste de média", function() {
+//     it("Resultado", function(){
+//         let res = aluno();
+//         expect(res).toEqual("Reprovado")
+//     })
+// })
+
+// describe("Pessoas", function(){
+//     let nome = "Adriano Sormano"
+//     let sobrenome = "Gomes da Silva Filho"
+//     let idade = 17
+
+//     let pessoa = new Pessoa(nome, sobrenome, idade);
+
+//     it('Teste - Nome', function(){
+//         expect("Adriano Sormano").toBe(pessoa.nome())
+//     })
+//     it('Teste - Nome e Sobrenome', function(){
+//         expect("Adriano Sormano Gomes da Silva Filho").toBe(pessoa.nome_completo())
+//     })
+//     it('Teste - Nome, Sobrenome e Idade', function(){
+//         expect("Adriano Sormano Gomes da Silva Filho tem idade de 17").toBe(pessoa.nome_tudo())
+//     })
+// })
+
+//ex1
+// describe ("multiplição", function() {
+//     it("Produto", function(){
+//         let res = multiplicacao();
+//         expect(res).toBe(24);
+//     })
+// });
+
+//ex2
+// describe("divisão", function(){
+//     it("resultado", function(){
+//         expect(divisao(10,2)).toBe(5);
+//     })
+// })
+
+//ex3
+ // describe ("Fahreinheit", function() {
+//     it ("Resultado", function() {
+//     let res = fahrenheit()
+//     expect(res). toBe(32)
+//     })
+// })
+// describe ("Celsius", function(){
+//     it ("Resultado", function(){
+//     let res = celsius()
+//     expect(res).toBe(25.6)
+//     })
+// })
+
+//ex4
+// describe ("Media", function() {
+//     it("Resultado", function(){
+//     let res = calcularMedia()
+//     expect(res).toBe(6)
+//     })
+// })
+
+//ex5
+// it ("Contar caracteres", function(){
+//     expect(contarCaracter ("hello")).toBe(5);
+//     expect(contarCaracter ("12345")).toBe(5);
+// })
+
+//ex6
+// it ("Calculadora", function(){
+//     expect(calculadora(4,2,"+")).toBe(6);
+//     expect(calculadora(4,2,"/")).toBe(2);
+// })
---
+++ jasmine-standalone-5.4.0/src/teste.js
@@ -0,0 +1,130 @@
+//teste.js é onde coloca o código fonte
+// 
+// function inteiro(inteiro) {
+//     return inteiro; //solução mais simples para o erro
+// }
+
+
+
+// fizz buzz
+// function jogo(numero){
+    
+//     if(numero %3 == 0){
+//         return "Fizz"
+//     }
+
+//     if(numero %5 == 0){
+//         return "Buzz"
+//     }
+
+//     return numero;
+// }
+
+// const aluno = (nota, media) =>{
+//     let resultado;
+//     let desempenho = 3;
+//     if(desempenho >= 7) {
+//         resultado = "Aprovado"
+//     } else {
+//         resultado = "Reprovado"
+//     }
+
+//     return resultado;
+// }
+
+// class Pessoa {
+//     constructor(_Nome, _Sobrenome, _Idade){
+//         let nome = _Nome;
+//         let sobrenome = _Sobrenome;
+//         let idade = _Idade;
+
+//         this.nome =() =>{
+//             return nome
+//         }
+//         this.nome_completo = () =>{
+//             return nome + ' ' + sobrenome
+//         }
+//         this.nome_tudo = () =>{
+//             return nome + ' ' + sobrenome + 'tem idade de' + idade;
+//         }
+// 
+
+//ex1
+// const multiplicacao = (numero) =>{
+//         let numero1 = 2;
+//         let numero2 = 3;
+//         let numero3 = 4;
+//         let produto;
+        
+//         if(numero1>= 0) {
+//             produto = numero1 * numero2 * numero3
+//         } else {
+//            produto = "Não é possível resolver"
+//         }
+    
+//         return produto;
+//     }
+
+//ex2
+// function divisao(numero){
+//     let resultado
+//     let numero1
+//     let numero2
+
+//     if(numero1 > 0){
+//         resultado = numero1/numero2
+//     } else {
+//         resutado = "Não é possível fazer divisão com 0"
+//  };
+// return resultado;
+// }
+
+//Ex З:
+// function fahrenheit(temperatura = 0){
+
+// let resultado = temperatura * 1.8 + 32
+
+// return resultado
+// }
+// function celsius(temperaturas = 32){
+// let resultado = temperaturas * 1.8 - 32
+// return resultado
+// }
+
+// ex 4:
+// function calcularMedia(num){
+// let num1 = 4
+// let num2 = 6
+// let num3 = 8
+
+// let resultado = num1 + num2 + num3
+
+
+// return resultado/3
+// }
+
+
+// ex 5
+
+// function contarCaracter (palavra){
+// let resultado = palavra.length
+// return resultado;
+// }
+
+// ex 6
+
+// function calculadora(numero1, numero2, operador) {
+//     switch (operador){
+//     case "+":
+//     return numero1 + numero2
+//     case "-":
+//     return numero1 - numero2
+//     case "*":
+//     return numero1 * numero2
+//     case "/":
+//     return numero1 / numero2
+   
+//     default:
+//     return "operador inválido"
+//     }
+// }
